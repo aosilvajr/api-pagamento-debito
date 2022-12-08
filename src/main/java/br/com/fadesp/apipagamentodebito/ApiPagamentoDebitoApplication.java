ee37e95987a16a41c82c5566ed8e04703f07c356
Ailton Jr <aosilvajr@gmail.com>
2022-12-08 19:38:39 -0300

    chore: adiciona estrutura inicial do projeto

---
+++ src/main/java/br/com/fadesp/apipagamentodebito/ApiPagamentoDebitoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fadesp.apipagamentodebito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPagamentoDebitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPagamentoDebitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fadesp/apipagamentodebito/ApiPagamentoDebitoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fadesp.apipagamentodebito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPagamentoDebitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
